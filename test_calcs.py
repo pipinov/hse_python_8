faef56c7c646b35a6cf0e5204b29051aa2352e7e
alexpipinov <alexpipinov@gmail.com>
2024-01-25 22:37:24 +0300

    fix: fix brocked test

--- test_calcs.py
+++ test_calcs.py
@@ -3,7 +3,7 @@ from calcs import sum
 def test_sum():
     assert sum(3, 4) == 7, "Test with integers failed"
     assert sum(2.5, 3.1) == 5.6, "Test with floats failed"
-    assert sum(-5, -7) == -13, "Test with negative numbers failed"
+    assert sum(-5, -7) == -12, "Test with negative numbers failed"
 
 if __name__ == "__main__":
     test_sum()
